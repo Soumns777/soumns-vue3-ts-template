54b383cdf2cdbf462a2eb8eb8986f9f39e8758ce
root <root@soumns.localdomain>
2022-05-25 22:59:30 +0800

    ✨ feat:addd-pinia-module

---
+++ src/store/modules/user.ts
@@ -0,0 +1,30 @@
+import { defineStore } from 'pinia';
+
+interface IPerson {
+  name: string;
+  age: number;
+}
+
+const useUser = defineStore('User', {
+  // arrow function recommended for full type inference
+  state: () => {
+    return {
+      user: {
+        name: 'iu',
+        age: 18,
+      },
+    };
+  },
+  getters: {
+    firstName: () => 'stephen',
+    nextName: () => 'curry',
+  },
+  actions: {
+    changeName(user: IPerson) {
+      this.user = user;
+      console.log('🌈 pinia响应式actions', this.user);
+    },
+  },
+});
+
+export default useUser;
--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,5 +1,21 @@
 <script lang="ts" setup>
 import { reactive, ref } from 'vue';
+
+import useStore from '@/store/index';
+import { storeToRefs } from 'pinia';
+import { useEverything } from '@/store/index';
+console.log(useEverything().User, 'useEverything 💙💛');
+
+const store = useStore().User;
+
+// 异步调用
+store.changeName({
+  name: 'yoona',
+  age: 23,
+});
+
+const { user, firstName, nextName } = storeToRefs(store);
+console.log(user, '💙💛 get-pinia');
 </script>
 
 <template>
