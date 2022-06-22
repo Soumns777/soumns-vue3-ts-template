933e24e1dd0010389b8796b98aa73c5d7465ef21
soumns <2794122209@qq.com>
2022-06-22 11:12:36 +0800

    ✨feat:add-unocss-vconsole

--- src/main.ts
+++ src/main.ts
@@ -2,14 +2,19 @@ import 'element-plus/theme-chalk/dark/css-vars.css';
 import { createPersistedState } from 'pinia-persistedstate-plugin';
 import App from './App.vue';
 
+import router from '@/router/index';
 import * as ElementPlusIconsVue from '@element-plus/icons-vue';
+import 'uno.css';
 
 const app = createApp(App);
 for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
   app.component(key, component);
 }
 
-import router from '@/router/index';
+// 引入vconsole
+import VConsole from 'vconsole';
+import { initPlugin } from 'vue-vconsole-devtools';
+initPlugin(new VConsole());
 
 const store = createPinia();
 store.use(createPersistedState());
--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
-
+import Unocss from 'unocss/vite';
+import { presetUno, presetAttributify, presetIcons } from 'unocss';
 import AutoImport from 'unplugin-auto-import/vite';
 import Components from 'unplugin-vue-components/vite';
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers';
@@ -26,6 +27,9 @@ export default defineConfig({
       include: [/\.vue$/, /\.vue\?vue/, /\.md$/],
       dts: 'src/components.d.ts',
       resolvers: [ElementPlusResolver()],
+    }),
+    Unocss({
+      presets: [presetUno(), presetAttributify(), presetIcons()],
     }),
   ],
   resolve: {
